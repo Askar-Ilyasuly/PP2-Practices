964ae61050714db11e1df954683548e29daa4169
Askar-Ilyasuly <a_ilyasuly@kbtu.kz>
2026-02-06 14:30:16 +0500

    Add practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice02/boolean/boolean_operators.py
@@ -0,0 +1,24 @@
+x = 10
+y = 5
+
+print(x > 5 and y < 10)
+
+a = 3
+b = 8
+
+print(a == 3 or b == 0) 
+
+
+logged_in = False
+
+print(not logged_in)
+
+
+age = 18
+
+print(age >= 18 and not age > 65)
+
+
+text = "hello"
+
+print("h" in text and "z" not in text) 
